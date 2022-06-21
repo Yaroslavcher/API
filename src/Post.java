0161ec18043a274a2e727f6f74bb557d40489219
Ярослав Черников <yaroslavcher@gmail.com>
2022-06-21 23:04:52 +0300

    first commit

---
+++ src/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; //поле для имени
+    public String patronymic; //поле для отчества
+    public String surname; //поле для фамилии
+    public String passport; //поле для серии и номера паспорта
+    public String phone; //поле для номера телефона
+    public boolean subscription; //поле для согласия на рассылку
+
+    public FormDate birthday; //поле для даты рождения
+}
